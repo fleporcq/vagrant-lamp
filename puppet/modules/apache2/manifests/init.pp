4f037b2ee594be494b9e9f413710307f7931e42f
François LEPORCQ <fleporcq@gmail.com>
2015-12-10 17:31:57 +0100

    feat(apache2): gestion du link pour user.conf

--- puppet/modules/apache2/manifests/init.pp
+++ puppet/modules/apache2/manifests/init.pp
@@ -1,21 +1,28 @@
 class apache2::install {
 
+  File {
+    ensure  => file,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    require => Package['apache2'],
+    notify  => Service['apache2'],
+  }
+  
   # install apache
   package { "apache2":
     ensure => present,
-    require => Exec["apt-get update"]
-  }
-
-
+    require => Exec["apt-get update"],
+  }->
   service { "apache2":
     ensure => running,
-    require => Package["apache2"]
   }
 
   # the httpd.conf change the user/group that apache uses to run its process
   file { '/etc/apache2/conf-available/user.conf':
     source  => '/vagrant/files/etc/apache2/user.conf',
   }
+  
   file { '/etc/apache2/conf-enabled/user.conf':
     ensure => link,
     target => '/etc/apache2/conf-available/user.conf',
@@ -25,6 +32,5 @@ class apache2::install {
   file { "/etc/apache2/mods-enabled/rewrite.load":
     ensure => link,
     target => "/etc/apache2/mods-available/rewrite.load",
-    require => Package["apache2"]
   }
 }
