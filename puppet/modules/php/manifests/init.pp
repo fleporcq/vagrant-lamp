efc123d3e9358b79808ffaa27fb5d7971b6df6a7
François LEPORCQ <fleporcq@gmail.com>
2015-12-16 14:39:10 +0100

    feat(php): modification du php.ini pour correspondre à un environnement de dev

--- puppet/modules/php/manifests/init.pp
+++ puppet/modules/php/manifests/init.pp
@@ -14,4 +14,17 @@ class php::install {
     ensure => present,
     require => Exec["apt-get update"]
   }
+
+  augeas { 'set-php-ini-values':
+    context => '/files/etc/php5/apache2/php.ini',
+    changes => [
+      'set PHP/error_reporting "E_ALL | E_STRICT"',
+      'set PHP/display_errors On',
+      'set PHP/display_startup_errors On',
+      'set PHP/html_errors On',
+      'set Date/date.timezone Europe/Paris',
+    ],
+    require => Package['php5'],
+    notify  => Service['apache2'],
+  }
 }
